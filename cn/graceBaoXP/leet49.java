6ab4ad62c923bb8a4cced639963c7084130d2eba
gracebxp <gracebxp@gmail.com>
2019-06-07 19:13:09 +0800

    提交49

---
+++ cn/graceBaoXP/leet49.java
@@ -0,0 +1,33 @@
+package cn.graceBaoXP;
+
+import java.util.*;
+
+/**
+ * Group Anagrams
+ *
+ * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * Output:
+ * [
+ *   ["ate","eat","tea"],
+ *   ["nat","tan"],
+ *   ["bat"]
+ * ]
+ */
+public class leet49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) {
+            return new ArrayList();
+        }
+        Map<String, List> ans = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)) {
+                ans.put(key, new ArrayList());
+            }
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
+}
